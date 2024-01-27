86d6ff32541118c976f5bbedc6a8544a33eda341
austinnnnnnnnnn <austinr528@gamil.com>
2024-01-27 12:57:46 -0500

    adds src dir

---
+++ src/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+# Player movement
+@export var speed = 100
+@export var gravity = 200
+
+func horizontal_movement():
+	# if keys are pressed it will return 1 for ui_right, -1 for ui_left, and 0 for neither
+	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	# horizontal velocity which moves player left or right based on input
+	velocity.x = horizontal_input * speed
+
+#movement and physics
+func _physics_process(delta):
+	# vertical movement velocity (down)
+	velocity.y += gravity * delta
+	# horizontal movement processing (left, right)
+	horizontal_movement()
+	#applies movement
+	move_and_slide()
