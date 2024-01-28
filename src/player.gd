d0ae02256a117ab77758a9139bfa18aca1a79ea3
Devin Ragotzy <devin.ragotzy@gmail.com>
2024-01-27 22:59:22 -0500

    do a bunch of tilemap shit damn!

--- src/player.gd
+++ src/player.gd
@@ -3,13 +3,18 @@ extends CharacterBody2D
 # Player movement
 @export var speed = 100
 @export var gravity = 200
-@export var jump = -300
+@export var jump = -100
+
+signal animation_change(animation)
 
 func horizontal_movement():
 	# if keys are pressed it will return 1 for ui_right, -1 for ui_left, and 0 for neither
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	# horizontal velocity which moves player left or right based on input
-	velocity.x = horizontal_input * speed	
+	velocity.x = horizontal_input * speed
+	if velocity.x != 0:
+		emit_signal("animation_change", "walk")
+		
 
 #movement and physics
 func _physics_process(delta):
