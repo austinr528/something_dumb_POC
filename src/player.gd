fd03dfb81e5a0823e2f4ccf62fe2b85e4793d081
austinnnnnnnnnn <austinr528@gamil.com>
2024-01-31 16:46:21 -0500

    Fixs player angle and odd down hill skipping

--- src/player.gd
+++ src/player.gd
@@ -6,7 +6,8 @@ extends CharacterBody2D
 @export var jump = -175
 
 var curr_jump_pos = 0
-var last_dir_right
+var last_dir_right = true
+var direction = 0
 var jumping = false
 var sprite_angle = 0.0
 
@@ -15,14 +16,30 @@ signal animation_change(animation)
 func horizontal_movement(is_jump):
 	# if keys are pressed it will return 1 for ui_right, -1 for ui_left, and 0 for neither
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	last_dir_right = horizontal_input > 0
+	direction = horizontal_input
 	# horizontal velocity which moves player left or right based on input
 	velocity.x = horizontal_input * speed
+	print(velocity.x)
 	if !is_jump:
 		if velocity.x != 0:
 			$AnimatedSprite2D.play('walk')
+			_normalize_movement_to_slope()
 		else:
 			$AnimatedSprite2D.play('default')
+
+func _normalize_movement_to_slope():
+	if direction > 0:
+		if sprite_angle > 0:
+			# if we are going down hill we have to fix velocity.y to stay  attached
+			velocity.y = 100
+		else:
+			pass
+	elif direction < 0:
+		if sprite_angle > 0:
+			pass
+		else:
+			# if we are going down hill we have to fix velocity.y to stay  attached
+			velocity.y = 100
 
 #movement and physics
 func _physics_process(delta):
@@ -45,23 +62,25 @@ func _physics_process(delta):
 	
 	if !is_jumped && is_on_floor():
 		jumping = false
-	# horizontal movement processing (left, right)
+		
 	horizontal_movement(jumping)
 	
-	if last_dir_right:
+	# orient the character to face the correct direction
+	if direction > 0 :
 		$AnimatedSprite2D.flip_h = false
-	else:
+		last_dir_right = true
+	elif direction < 0:
 		$AnimatedSprite2D.flip_h = true
+		last_dir_right = false
 		
-	print(get_floor_angle(), ' ', get_floor_normal().angle())
+	# Determine the angle of the character
 	if is_on_floor():
-		var angle = get_floor_angle()
+		var angle = get_floor_normal().angle()
 		if angle != sprite_angle:
 			var angle_delta
-			if last_dir_right: angle_delta = (sprite_angle - angle)
-			else: angle_delta = (angle - sprite_angle)
-			$AnimatedSprite2D.rotate(angle_delta)
-			sprite_angle = angle
+			rotation = angle + (PI/2)
+			sprite_angle = rotation
+
 	else:
 		$AnimatedSprite2D.rotation = 0
 	#applies movement
