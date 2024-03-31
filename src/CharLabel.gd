d9fede8c713b31f7e05b358a20629cda740ee034
Devin Ragotzy <devin.ragotzy@gmail.com>
2024-03-30 20:33:38 -0400

    fix last debug var

--- src/CharLabel.gd
+++ src/CharLabel.gd
@@ -4,9 +4,9 @@ var label_ref : Label = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if Global.DEBUG:
+	if Global.DEBUG_ALL:
 		text = 'Default'
 
 func _on_character_animation_change(animation):
-	if Global.DEBUG:
+	if Global.DEBUG_ALL:
 		text = animation
